3b2dcc1bde546c7c02b919a30595ba3e43da86fe
Jake Rosoman <jkroso@gmail.com>
2015-09-21 17:49:14 +1200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# eslint-plugin-jsx
+
+JSX specific linting rules for ESLint. Designed for the semantics of the [JSX-to-JS](//github.com/jkroso/JSX-to-JS) transpiler and is not compatible with the standard JSX semantics.
+
+## Installation
+
+You need to install this plugin in the same place eslint itself is installed. So if your using a globally installed eslint this will also need to be installed globally
+
+`npm install -g eslint-plugin-jsx`
+
+then in your ~/.eslintrc:
+
+```json
+{
+  "plugins": ["jsx"],
+  "rules": {
+    "jsx/uses-factory": [1, {"pragma": "JSX"}],
+    "jsx/factory-in-scope": [1, {"pragma": "JSX"}],
+    "jsx/mark-used-vars": 1,
+    "jsx/no-undef": 1
+  }
+}
+```
+
+All those rules are defined by default though so you can leave out the ones you agree with.
+
+## Thanks
+
+This project is based of [yannickcr/eslint-plugin-react](//github.com/yannickcr/eslint-plugin-react). Which has some rules you will probably also be interested in.
---
+++ index.js
@@ -0,0 +1,105 @@
+const {findVariable, variablesInScope} = require('eslint-plugin-react/lib/util/variable')
+const htmlTags = require('html-tags')
+const svgTags = require('svg-tags')
+
+const standardTags = new Set(htmlTags.concat(svgTags))
+
+/**
+ * Check the factory function is imported if JSX is being used
+ */
+
+const factoryInScopeRule = context => {
+  const config = context.options[0] || {}
+  const name = config.pragma || 'JSX'
+  return {
+    JSXOpeningElement: function(node) {
+      var variables = variablesInScope(context)
+      if (findVariable(variables, name)) return
+      context.report({
+        message: `'${name}' must be in scope when using JSX syntax`,
+        node: node
+      })
+    }
+  }
+}
+
+factoryInScopeRule.schema = [{
+  type: 'object',
+  properties: {
+    pragma: {type: 'string'}
+  },
+  additionalProperties: false
+}]
+
+/**
+ * Prevent the JSX factory function from being marked as unused
+ * if it will be in the compiled source
+ */
+
+const usesFactoryRule = context => {
+  const config = context.options[0] || {}
+  const id = config.pragma || 'JSX'
+  return {
+    JSXElement() {
+      context.markVariableAsUsed(id)
+    }
+  }
+}
+
+usesFactoryRule.schema = factoryInScopeRule.schema
+
+/**
+ * Prevent variables used in JSX to be marked as unused
+ */
+
+const markUsedRule = context => {
+  return {
+    JSXOpeningElement(node) {
+      var name = node.name
+      if (name.type == 'JSXMemberExpression') name = name.object
+      if (name.type == 'JSXNamespacedName') name = name.namespace
+      context.markVariableAsUsed(name.name)
+    },
+    JSXAttribute(attr) {
+      if (attr.value == null) context.markVariableAsUsed(attr.name.name)
+    }
+  }
+}
+
+/**
+ * Disallow undeclared variables in JSX
+ */
+
+const noUndefRule = context => {
+  return {
+    JSXOpeningElement(node) {
+      var name = node.name
+      if (name.type == 'JSXMemberExpression') name = name.object
+      if (name.type == 'JSXNamespacedName') name = name.namespace
+      if (standardTags.has(name.name)) return
+      const variables = variablesInScope(context)
+      if (findVariable(variables, name.name)) return
+      context.report({
+       message: `'${name.name}' is not defined`,
+       node: name
+      })
+    }
+  }
+}
+
+const rules = {
+  'uses-factory': usesFactoryRule,
+  'factory-in-scope': factoryInScopeRule,
+  'mark-used-vars': markUsedRule,
+  'no-undef': noUndefRule
+}
+
+// enable all by default
+const rulesConfig = {
+  'uses-factory': 1,
+  'factory-in-scope': 1,
+  'mark-used-vars': 1,
+  'no-undef': 1
+}
+
+export {rules,rulesConfig}
